bd44cef27551eae999160e0e10332a7e3bf1d769
Yana Kapustian <yana2015.2004@gmail.com>
2022-07-28 15:08:45 +0300

    Homework 21

--- react-21-hw/src/components/Contacts.jsx
+++ react-21-hw/src/components/Contacts.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './Contacts.scss'
 import Contact from './Contact';
 
 const Contacts = () => {
-    const contacts = [{
+    const contactsInitial = [{
         firstName: "Барней",
         lastName: "Стинсовський",
         phone: "+380956319521",
@@ -34,62 +34,51 @@ const Contacts = () => {
         phone: "+380956319521",
         gender: "male"
     }];
-    const [state] = useState({
-        contacts,
-        render() {
-            return (
-                this.contacts.map(contact => <Contact props={contact} key={contact.firstName + contact.lastName} /> )
-            )
-        },
-    })
-    let [value, setValue] = useState('')
-    const handleSearchChange = (e) => {
-        setValue(value = e.target.value)
-        state.contacts = contacts.filter(contact => {
-            return contact.lastName.toLowerCase().includes(value.toLowerCase()) ||
-            contact.firstName.toLowerCase().includes(value.toLowerCase()) ||
-            contact.phone.toLowerCase().includes(value.toLowerCase())
-        })
-        state.render()
-    }
-    
+    const [contacts] = useState(contactsInitial)
+    let [filteredContacts, setFilteredContacts] = useState([])
+    let [searchValue, setSearchValue] = useState('')
+
     const refMale = useRef(null);
     const refFemale = useRef(null);
     const refNotMentioned = useRef(null);
+    
+    const handleSearchChange = (e) => {
+        setSearchValue(searchValue = e.target.value)
+    }
 
-    let [array, setArray] = useState([]);
+    let array = [];
 
-    const handleCheckbox = (e) => {
-            if (e.target.className !== 'form-check-input' && e.target.className !== 'form-check-label') return;
-            let array = [];
-            if (refMale.current.checked) setArray(array.push("male"))
-            if (refFemale.current.checked) setArray(array.push("female"))
-            if (refNotMentioned.current.checked) setArray(array.push("not-mentioned"))
-            switch(array.length) {
-                case 0:
-                    state.contacts = [];
-                    break;
-                case 1:
-                    state.contacts = contacts.filter(contact => {
-                        return contact.gender === array[0];
-                    })
-                    break;
-                case 2:
-                    state.contacts = contacts.filter(contact => {
-                        return contact.gender === array[0] || contact.gender === array[1]
-                    })
+    const handleCheckbox = () => {
+        if (refMale.current.checked) array.push("male")
+        if (refFemale.current.checked) array.push("female")
+        if (refNotMentioned.current.checked) array.push("not-mentioned")
 
-                    break;
-                case 3:
-                    state.contacts = contacts;
-                    break;
-            }
-            state.render()
+        setFilteredContacts(filteredContacts = contacts.filter(contact => {
+            return contact.gender === array[0] || contact.gender === array[1] || contact.gender === array[2]
+        }).filter(contact => {
+            return contact.lastName.toLowerCase().includes(searchValue.toLowerCase()) ||
+            contact.firstName.toLowerCase().includes(searchValue.toLowerCase()) ||
+            contact.phone.toLowerCase().includes(searchValue.toLowerCase())
+        }) )
+    }
+
+    useEffect(() => {
+        handleCheckbox()
+    }, [searchValue])
+
+    const checkLength = () => {
+        if (filteredContacts.length === 0) {
+            return (
+                <div>
+                    <h3 className='not-found'>Контактів не знайдено</h3>
+                </div>
+            )
+        }
     }
 
     return (
         <div>
-            <div className='header' onClick={handleCheckbox}>
+            <div className='header'>
                 <div className="input-group mb-3">
                     <input onChange={handleSearchChange} type="text" className="form-control" placeholder="Введіть дані для пошуку" aria-label="Recipient's username" aria-describedby="basic-addon2"></input>
                     <div className="input-group-append">
@@ -97,19 +86,20 @@ const Contacts = () => {
                     </div>
                 </div>
                 <div className="form-check form-switch">
-                    <input ref={refMale} className="form-check-input" type="checkbox" id="male" defaultChecked></input>
+                    <input ref={refMale} className="form-check-input" type="checkbox" id="male" defaultChecked onClick={handleCheckbox}></input>
                     <label className="form-check-label" htmlFor="male">Ч</label>
                 </div>
                 <div className="form-check form-switch">
-                    <input ref={refFemale} className="form-check-input" type="checkbox" id="female" defaultChecked></input>
+                    <input ref={refFemale} className="form-check-input" type="checkbox" id="female" defaultChecked onClick={handleCheckbox}></input>
                     <label className="form-check-label" htmlFor="female">Ж</label>
                 </div>
                 <div className="form-check form-switch">
-                    <input ref={refNotMentioned} className="form-check-input" type="checkbox" id="not-mentioned" defaultChecked></input>
+                    <input ref={refNotMentioned} className="form-check-input" type="checkbox" id="not-mentioned" defaultChecked onClick={handleCheckbox}></input>
                     <label className="form-check-label" htmlFor="not-mentioned">не вказано</label>
                 </div>
             </div>
-            {state.render()}
+            {filteredContacts.map(contact => <Contact props={contact} key={contact.firstName + contact.lastName} /> )}
+            {checkLength()}
         </div>
     )
 }
